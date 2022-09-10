98f9479e76a71cf638258c1e18e81f64edb5bb66
Marco Retamales <marcoret74@gmail.com>
2022-09-10 15:49:16 -0400

    Proyect inicial listo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navigation } from './routes/Navigation';
+
+function App() {
+  return (
+    <Navigation/>
+  );
+}
+
+export default App;
